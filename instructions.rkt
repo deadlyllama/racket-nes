76f82a47b92d3a0aa1531003f2c1c6a1e6280196
Laura Leppänen <laura.leppanen@iki.fi>
2015-07-30 18:39:39 +0300

    Implement LSR (Logical Shift Right)

--- instructions.rkt
+++ instructions.rkt
@@ -18,12 +18,24 @@
 (define (asl a-state a-value amount)
   (let* ([shifted (arithmetic-shift a-value amount)]
          [masked (bitwise-and #xff shifted)]
-         [flags (state-p a-state)]
-         [new-flags (if (bitwise-bit-set? shifted 8)
+         [carry (bitwise-bit-set? shifted 8)]
+         [new-state (state-set-carry a-state carry)])
+    (struct-copy state new-state
+                 [a masked])))
+
+(define (lsr a-state a-value amount)
+  (let* ([shifted (arithmetic-shift a-value (- amount))]
+         [carry (bitwise-bit-set? a-value 0)]
+         [new-state (state-set-carry a-state carry)])
+    (struct-copy state new-state
+                 [a shifted])))
+
+(define (state-set-carry a-state carry)
+  (let* ([flags (state-p a-state)]
+         [new-flags (if carry
                         (bitwise-ior flags 1)
                         (bitwise-and flags (bitwise-not 1)))])
     (struct-copy state a-state
-                 [a masked]
                  [p new-flags])))
 
 (define instructions
